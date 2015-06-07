4f0607fadc6f1487f31d1943b655546f0d6aed46
andela-ooduye <oduyeoluwayemisi@andela.co>
2015-06-07 08:32:44 +0100

    refactored routes

--- app/controller/controller.user.js
+++ app/controller/controller.user.js
@@ -4,7 +4,7 @@ var User = require('../models/user');
  * @type {Object}
  */
 module.exports = {
-  
+
   getAllUsers: function(req, res) {
 
     // use mongoose to get all users in the database
@@ -23,19 +23,21 @@ module.exports = {
    * @return {[void]}
    */
   createNewUser: function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.json(req.user);
+  },
 
-    // create a user, information comes from AJAX request from Angular
-    User.create({
-      userName: req.body.userName,
-      password: req.body.password,
-      email: req.body.email
-    }, function(err, user) {
-      if (err)
-        res.send(err);
+  loginUser: function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.json(req.user)
+  },
 
-        res.json({message: "user has been created"});
-      });
-    },
+  logoutUser: function(req, res) {
+    req.logout();
+    res.send('user has been logged out');
+  },
   /**
    * [editUser editis a particular user in the database]
    * @param  {[req]}
@@ -55,7 +57,9 @@ module.exports = {
       user.save(function(err, users) {
         if (err)
           res.send(err)
-        res.json({data: users});
+        res.json({
+          data: users
+        });
       });
     });
   },
@@ -88,6 +92,17 @@ module.exports = {
    */
   otherwise: function(req, res) {
     res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+  },
+
+  // route middleware to make sure a user is logged in
+  isLoggedIn: function(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+      return next();
+
+    // if they aren't redirect them to the home page
+    res.send('unauthorized');
   }
 
 };
--- app/route/routes.js
+++ app/route/routes.js
@@ -10,57 +10,37 @@ module.exports = function(app, passport) {
   // get all recipes
   app.get('/api/recipes', recipe.getAllRecipes);
   app.get('/api/recipes/:recipe_id', recipe.getRecipe);
-  app.get('/api/users/', isLoggedIn, user.getAllUsers);
+  app.get('/api/users/', user.isLoggedIn, user.getAllUsers);
 
   // create recipe and send back all recipes after creation
-  app.post('/api/recipes', isLoggedIn, recipe.createNewRecipe);
-  // app.post('/api/users', user.createNewUser);
+  app.post('/api/recipes', user.isLoggedIn, recipe.createNewRecipe);
+  
+  // create a new user, process the signup form
+  app.post('/api/signup', passport.authenticate('local-signup'), user.createNewUser);
+
+  app.post('/api/login', passport.authenticate('local-login'), user.loginUser);
+
+  app.get('/api/logout', user.logoutUser);
+
 
   // edit a recipe
-  app.put('/api/recipes/:recipe_id', recipe.editRecipe);
-  app.put('/api/users/:user_id', user.editUser);
+  app.put('/api/recipes/:recipe_id', user.isLoggedIn, recipe.editRecipe);
+  app.put('/api/users/:user_id', user.isLoggedIn, user.editUser);
 
 
   // delete a recipe
-  app.delete('/api/recipes/:recipe_id', recipe.deleteRecipe);
-  app.delete('/api/users/:user_id', user.deleteUser);
+  app.delete('/api/recipes/:recipe_id', user.isLoggedIn, recipe.deleteRecipe);
+  app.delete('/api/users/:user_id', user.isLoggedIn, user.deleteUser);
 
   // login a user
   // app.post('/api/login', passport.authenticate('local-signup', {
   //   successRedirect: res.json({message: 'you have been logged in'});
   // }))
 
-  // process the signup form
-  app.post('/api/signup',
-    passport.authenticate('local-signup'),
-    function(req, res) {
-      // If this function gets called, authentication was successful.
-      // `req.user` contains the authenticated user.
-      res.json(req.user);
-    });
-
-  app.post('/api/login', passport.authenticate('local-login'), function(req, res) {
-    res.json(req.user)
-  });
-
-  app.get('/api/logout', function(req, res) {
-    req.logout();
-    res.send('user has been logged out');
-  });
-
+  
 
   app.get('*', recipe.otherwise);
   app.get('*', user.otherwise);
 
-  // route middleware to make sure a user is logged in
-  function isLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on 
-    if (req.isAuthenticated())
-      return next();
-
-    // if they aren't redirect them to the home page
-    res.send('unauthorized');
-  }
 
 };
